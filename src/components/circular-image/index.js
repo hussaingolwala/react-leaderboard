017fbd0908b6cfa1e7415169a1af1013a1e3a06d
hussaingolwala <70024663+hussaingolwala@users.noreply.github.com>
2022-06-21 19:29:49 +0530

    Update index.js

--- src/components/circular-image/index.js
+++ src/components/circular-image/index.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const Content = styled.img`
-width: 50px;
-height: 50px;
+width: 3rem;
+height: 3rem;
 border-radius: 100%;
-margin-left: 2.5%;
+margin-left: 2%;
 display: flex;
 overflow: hidden;
 justify-content: center;
 align-items: center;
 src: url(${props => props.src});
 `;
+
+
