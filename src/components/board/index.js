bf41bedec0124b6d546c31c5ef22a4894a2be021
hussaingolwala <70024663+hussaingolwala@users.noreply.github.com>
2022-06-21 18:33:22 +0530

    Update index.js

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import { Content } from "../circular-image";
-import Img from "../assets/3.png";
+import { Content} from "../circular-image";
 import { useState, useEffect } from "react";
 import { data } from "../../data";
 
@@ -22,7 +21,7 @@ const Div = styled.div`
 const Name = styled.div`
   display: flex;
   justify-content: center;
-  font-size: 25px;
+  font-size: 22px;
   margin-left: 1.5%;
   font-weight: 450;
   color: ${(props) => (props.color === "black" ? "black" : "white")};
@@ -32,64 +31,65 @@ const Points = styled.div`
   display: flex;
   justify-content: flex-end;
   flex: 1;
-  font-size: 25px;
+  font-size: 22px;
   font-weight: 600;
   letter-spacing: 1.5px;
-  margin-right: 1%;
+  margin-right: 0.5%;
   color: ${(props) => (props.color === "black" ? "black" : "white")};
 `;
+
 
 const Point = styled.div`
   display: flex;
   justify-content: center;
-  font-size: 25px;
+  font-size: 22px;
   font-weight: 200;
-  padding-right: 4%;
+  padding-right: 1.5%;
   color: ${(props) => (props.color === "black" ? "black" : "white")};
 `;
 
 function Board() {
-//   const [ /* count, */  setCount] = useState(46200);
-
-//   useEffect(() => {
-//     const id = setInterval(() => setCount((oldCount) => oldCount + 100), 1000);
-
-//     return () => {
-//       clearInterval(id);
-//     };
-//   });
-
-const [num, setNum] = useState([]);
+  const [num, setNum] = useState([]);
+  const [dataOne, setDataOne] = useState(data);
 
   function randomNumberInRange(max) {
-    return Math.floor(Math.random() * (max + 100));
+    return Math.floor(Math.random() * max) + 100;
   }
 
-  useEffect(() => {
-    const id = setInterval(() => setNum(randomNumberInRange(157000, 1000000)), 1000);
+  const getRandomUser = () => {
+    return Math.floor(Math.random() * (data.length));
+  };
 
-        return () => {
-          clearInterval(id);
-        };
-    // setNum(randomNumberInRange(1, 5));
-  });
+  console.log(num,'num');
+
+  useEffect(() => {
+    const updatedData = [...data];
+    updatedData.sort((a, b) => b.score - a.score);
+    setDataOne(updatedData);
+
+    const id = setInterval(() => {
+      setNum(randomNumberInRange(1000, 100000));
+      const selectedUser = getRandomUser();
+      let updatedData = [...data];
+      updatedData[selectedUser].score = Number(data[selectedUser].score) + num;
+      updatedData.sort((a, b) => b.score - a.score);
+      setDataOne(updatedData);  
+    }, 500);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [num]);
 
   return (
     <>
-        <Container>
-          <Div mt="0.5%">
-            <Content src={Img} />
-            <Name color="white">Jone</Name>
-            <Points color="white">{num}</Points>
-            <Point color="white">points</Point>
-          </Div>
-        </Container>
-      {data.map((userID) => (
-        <Container>
-          <Div mt="0.5%" bg="white" key={userID}>
-            <Content src={Img} />
+      {dataOne.map((userID) => (
+        <Container key={userID}>
+          <Div mt="0.5%" bg="white"> 
+            <Content src={userID.picture}>
+            </Content>
             <Name color="black">{userID.displayName}</Name>
-            <Points color="black">{num}</Points>
+            <Points color="black">{userID.score}</Points>
             <Point color="black">points</Point>
           </Div>
         </Container>
